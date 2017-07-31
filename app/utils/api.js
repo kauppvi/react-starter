08963a113fe7d0db83ff88d08b392c208f4c1c63
Ville Kauppi <kauppvi@gmail.com>
2017-07-31 09:30:26 +0300

    Vantaa-job-board-ize

--- app/utils/api.js
+++ app/utils/api.js
@@ -3,7 +3,7 @@ var axios = require('axios');
 module.exports = {
   fetchJobOpenings: function (field){
     //var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
-    var encodedURI = window.encodeURI('http://gis.vantaa.fi/rest/tyopaikat/v1/'+field);
+    var encodedURI = window.encodeURI('https://gis.vantaa.fi/rest/tyopaikat/v1/'+field);
 
     return axios.get(encodedURI)
       .then(function (response) {
--- docs/index_bundle.js
+++ docs/index_bundle.js
@@ -10944,7 +10944,7 @@ var axios = __webpack_require__(95);
 module.exports = {
   fetchJobOpenings: function fetchJobOpenings(field) {
     //var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
-    var encodedURI = window.encodeURI('http://gis.vantaa.fi/rest/tyopaikat/v1/' + field);
+    var encodedURI = window.encodeURI('https://gis.vantaa.fi/rest/tyopaikat/v1/' + field);
 
     return axios.get(encodedURI).then(function (response) {
       // return response.data.items;
