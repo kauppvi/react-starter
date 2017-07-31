9ccd114128e4e7e8cd9f2d3b6b12121b209729e1
V Kauppi <kauppvi@gmail.com>
2017-07-31 13:30:13 +0300

    Cleanup

--- app/utils/api.js
+++ app/utils/api.js
@@ -2,12 +2,10 @@ var axios = require('axios');
 
 module.exports = {
   fetchJobOpenings: function (field){
-    //var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
     var encodedURI = window.encodeURI('https://gis.vantaa.fi/rest/tyopaikat/v1/'+field);
 
     return axios.get(encodedURI)
       .then(function (response) {
-        // return response.data.items;
         return response.data;
       });
   }
